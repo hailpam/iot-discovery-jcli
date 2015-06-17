6071fbc9c26e918c0697849c638d3b44ec901471
Paolo Maresca <pmaresca@verisign.com>
2015-06-17 10:57:18 +0200

    Refactoring \
    Got rid of the testing code

--- src/main/java/com/verisign/iot/discovery/cli/Launcher.java
+++ src/main/java/com/verisign/iot/discovery/cli/Launcher.java
@@ -1,10 +1,10 @@
 
 package com.verisign.iot.discovery.cli;
 
+import com.verisign.iot.discovery.cli.common.ExitCodes;
 import com.verisign.iot.discovery.cli.console.DefaultConsoleWriter;
 import com.verisign.iot.discovery.cli.exception.CommandNotFoundException;
 import com.verisign.iot.discovery.cli.exception.ExecutionException;
-import com.verisign.iot.discovery.cli.common.ExitCodes;
 import com.verisign.iot.discovery.cli.exception.OptionsNotValidException;
 import com.verisign.iot.discovery.cli.exception.ParsingException;
 import com.verisign.iot.discovery.cli.parser.DefaultOptionParser;
@@ -12,29 +12,24 @@ import com.verisign.iot.discovery.cli.parser.Options;
 import joptsimple.OptionSet;
 
 /**
- * Launcher application. This class contains the main method to start up the 
+ * Launcher application. This class contains the main method to start up the
  * command line application.
- * 
+ *
  * @author nbrasey <nbrasey@verisign.com>
  * @version 1.0
  * @since 4/30/15.
  */
-public class Launcher 
+public class Launcher
 {
 
-	public static void main ( String[] args ) 
+	public static void main ( String[] args )
     {
 
 		ConsoleWriter consoleWriter = new DefaultConsoleWriter();
-
 		CommandOptionParser parser = new DefaultOptionParser();
-
-//        String test[] = { "-d", "com", "-t", "example", "-n", "8.8.8.8" };
-
 		try {
 			// Parse the arguments
 			OptionSet optionSet = parser.parse( args );
-//            OptionSet optionSet = parser.parse( test );
 
 			// Build the specific command from the parsed arguments
 			Command command = CommandFactory.buildCommand( optionSet );
@@ -71,5 +66,5 @@ public class Launcher
 			System.exit( ExitCodes.GENERIC_ERROR.getExitCode() );
 		}
 	}
-    
+
 }
